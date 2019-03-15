3c06844745ca3daee9f2eebe030ce1c87b21e1e6
Danny <531194759@qq.com>
2019-03-15 07:22:04 +0000

    测试验证错误

--- tests/Feature/CreateThreadsTest.php
+++ tests/Feature/CreateThreadsTest.php
@@ -22,16 +22,53 @@ class CreateThreadsTest extends TestCase
         $this->post('/threads')
             ->assertRedirect('/login');
     }
+    /** @test */
+    public function a_thread_requires_a_title()
+    {
+        $this->publishThread(['title' => null])
+            ->assertSessionHasErrors('title');
+    }
 
+    /** @test */
+    public function a_thread_requires_a_body()
+    {
+        $this->publishThread(['body' => null])
+            ->assertSessionHasErrors('body');
+    }
+
+    /** @test */
+    public function a_thread_requires_a_valid_channel()
+    {
+        factory('App\Channel',2)->create(); // 新建两个 Channel，id 分别为 1 跟 2
+
+        $this->publishThread(['channel_id' => null])
+            ->assertSessionHasErrors('channel_id');
+
+        $this->publishThread(['channel_id' => 999])  // channle_id 为 999，是一个不存在的 Channel
+        ->assertSessionHasErrors('channel_id');
+    }
+
+    public function publishThread($overrides = [])
+    {
+        $this->withExceptionHandling()->signIn();
+
+        $thread = make('App\Thread',$overrides);
+
+        return $this->post('/threads',$thread->toArray());
+    }
     /** @test */
     public function an_authenticated_user_can_create_new_forum_threads()
     {
-        $this->signIn();
+        // Given we have a signed in user
+        $this->signIn();  // 已登录用户
 
-        $thread = create('App\Thread');
-        $this->post('/threads',$thread->toArray());
+        // When we hit the endpoint to cteate a new thread
+        $thread = make('App\Thread');
+        $response = $this->post('/threads',$thread->toArray());
 
-        $this->get($thread->path())
+        // Then,when we visit the thread
+        // We should see the new thread
+        $this->get($response->headers->get('Location'))
             ->assertSee($thread->title)
             ->assertSee($thread->body);
     }
--- tests/Feature/ParticipateInForumTest.php
+++ tests/Feature/ParticipateInForumTest.php
@@ -36,4 +36,15 @@ class ParticipateInForumTest extends TestCase
         $this->get($thread->path())
             ->assertSee($reply->body);
     }
+    /** @test */
+    public function a_reply_reqiures_a_body()
+    {
+        $this->withExceptionHandling()->signIn();
+
+        $thread = create('App\Thread');
+        $reply = make('App\Reply',['body' => null]);
+
+        $this->post($thread->path() . '/replies',$reply->toArray())
+            ->assertSessionHasErrors('body');
+    }
 }
